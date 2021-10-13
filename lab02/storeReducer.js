93b55fd2b78816773b3fefed1b5f097f7a8393c3
Darusia <dariapionk13@gmail.com>
2021-10-13 13:37:08 +0200

    Zadanie 2

--- lab02/storeReducer.js
+++ lab02/storeReducer.js
@@ -1,6 +1,6 @@
 const Redux = require('redux');
 
-function storeReducer(state = { todos: [] }, action) {
+function storeReducer(state = { todos: [], notes: [] }, action) {
     switch(action.type) {
         case 'ADD_TODO':
             return { ...state, todos: [...state.todos, action.payload ]};
@@ -9,7 +9,7 @@ function storeReducer(state = { todos: [] }, action) {
         case 'FINISH_TODO':
             return { ...state, todos: [...state.todos.map((element) => {
                 if (element.id == action.payload.id) {
-                    nowyElement = element
+                    let nowyElement = element
                     nowyElement.done = true
                     return nowyElement
                 }
@@ -18,14 +18,27 @@ function storeReducer(state = { todos: [] }, action) {
                 }
             })] };
         case 'UPDATE_TODO': 
-            return {...state.todos.map((element) => {
+            return { ...state, todos : [...state.todos.map((element) => {
                 if (element.id == action.payload.id) {
-                    return action.payload
+                    let nowyElement = element
+                    nowyElement.title = action.payload.title
+                    nowyElement.done = action.payload.done
+                    return nowyElement
                 }
                 else {
                     return element
                 }
-            })};
+            })]};
+        case 'ADD_NOTE':
+            {
+                return {
+                    ...state,  notes: [ ...state.notes, action.payload ]
+                }
+            }
+        case 'DELETE_NOTE':
+            {
+                return { ...state, notes: [...state.notes.filter(el => el.id != action.payload.id)]}
+            }
         default:
             return state;
     }
@@ -33,5 +46,15 @@ function storeReducer(state = { todos: [] }, action) {
 
 
 
-let store = Redux.createStore(storeReducer);
-store.dispatch({ type: 'ADD_TODO', payload: { id: 0, title: 'test', done: false } });
+// let store = Redux.createStore(storeReducer);
+// store.dispatch({ type: 'ADD_TODO', payload: { id: 0, title: 'test', done: false } });
+// store.dispatch({ type: 'ADD_TODO', payload: { id: 1, title: 'test1', done: false } });
+// store.dispatch({ type: 'ADD_TODO', payload: { id: 2, title: 'test2', done: false } });
+
+// store.dispatch({ type: 'UPDATE_TODO', payload: { id: 0, title: 'test2fdgfdgfdg', done: false } });
+// store.dispatch({ type: 'FINISH_TODO', payload: { id: 2, title: 'test2fdgfdgfdg', done: false } });
+// store.dispatch({ type: 'DELETE_TODO', payload: { id: 2, title: 'test2fdgfdgfdg', done: false } });
+// store.dispatch({ type: 'ADD_NOTE', payload: { id: 1, content: 'asdfesfgdrg'} });
+// store.dispatch({ type: 'ADD_NOTE', payload: { id: 2, content: 'asdfesfgfdgfddrg'} });
+// // store.dispatch({ type: 'DELETE_NOTE', payload: { id: 2, content: 'asdfesfgfdgfddrg'} });
+// console.log(store.getState())
